f7aa742427a30f88e2ad86967591fbc75998860a
321BadgerCode <321badger.code@gmail.com>
2025-06-15 19:58:17 -0400

    - ✨ Update commit filename.

--- setup_dev.sh
+++ setup_dev.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-git config commit.template ./git_commit_template.txt
+git config commit.template ./.git_commit_template.txt
 mkdir -p ./.git/hooks
 cp ./.git_hooks/* ./.git/hooks/
 chmod +x ./.git/hooks/*
